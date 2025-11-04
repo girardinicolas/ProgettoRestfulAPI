0e5d563c458c8156f75710b8a6894f9c3ca12de6
nicolas <girardinicolas3@gmail.com>
2025-11-04 16:59:51 +0100

    Updated navigation and home component structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Importa i componenti delle pagine
+import { HomeComponent } from '../app/components/home/home.component';
+import { ListPokeComponent } from './components/list-poke/list-poke.component';
+import { CreatePokeComponent } from './components/create-poke/create-poke.component';
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // reindirizza alla home di default
+  { path: 'home', component: HomeComponent },
+  { path: 'list-poke', component: ListPokeComponent },
+  { path: 'create-poke', component: CreatePokeComponent },
+  { path: '**', redirectTo: '/home' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
